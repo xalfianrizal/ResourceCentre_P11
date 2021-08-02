c82d8689107b129f4bba67f0b2694aba49020479
20026062 <20026062@DESKTOP-FVA8ADO>
2021-08-02 12:59:18 +0800

    Updated toString method, Added showAvailability method

--- ResourceCentre_P11/src/Item.java
+++ ResourceCentre_P11/src/Item.java
@@ -12,9 +12,22 @@ public class Item {
 		this.isAvailable = true;
 	}
 	public String toString() {
+		String itemInfo = String.format("%-10s %-30s %-10s %-10s", assetTag,
+				description, 
+				showAvailability(isAvailable),
+				dueDate);
 		
-		// Write your codes here
-		return null;
+		return itemInfo;
+	}
+	public String showAvailability(boolean isAvailable) {
+		String avail;
+
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
 	}
 	public String getAssetTag() {
 		return assetTag;
